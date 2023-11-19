56869c26438497a865bb7847e1ba2ed34a57bb12
Danila <gamer.xokks@yandex.ru>
2023-11-19 15:30:45 +0300

    improve config

---
+++ config.py
@@ -0,0 +1,7 @@
+import json
+data = 0
+def load_configurate(file:str):
+    global data
+    with open(file) as f:
+        data = json.load(f)  
+
--- start.py
+++ start.py
@@ -1,20 +1,12 @@
 from flask import Flask, render_template
-import json
+import config
 
 app = Flask(__name__)
-
-config = 0
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-def load_configurate(file:str):
-    global config
-    with open(file) as f:
-        config = json.load(f)  
-
 if __name__ == '__main__':
-    load_configurate('config.json')
-    app.run(port=int(config['server_port']), host=str(config['server_host']), debug=bool(config['server_debug']))
+    config.load_configurate('config.json')
+    app.run(port=int(config.data['server_port']), host=str(config.data['server_host']), debug=bool(config.data['server_debug']))
