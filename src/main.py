127c10ab118e738db54d9606bf4d20c01fad3cd8
Danila <gamer.xokks@yandex.ru>
2023-11-26 16:03:30 +0300

    fix path and run app

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-import config as config #src.config to config
 
 app = Flask(__name__)
 
@@ -14,11 +13,13 @@ def login():
         password = request.form['password']
         
         # Здесь может быть проверка данных пользователя
-
-        return f'Добро пожаловать, {username}!'
+             
+        if request.form['submit_button'] == 'login':
+            return f'Добро пожаловать, {username}!'
+        
+        if request.form['submit_button'] == 'register':
+            return f'Создана учетная запись с именем, {username}!'
+        
+        return f'Неверные параметры POST'
     
     return render_template('login.html')
-
-if __name__ == '__main__':
-    config.load_configurate('config.json')
-    app.run(port=int(config.data['server_port']), host=str(config.data['server_host']), debug=bool(config.data['server_debug']))
