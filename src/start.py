91c483f4f4048cca43e698fa1ccdfbfc1ea16f6a
Danila <gamer.xokks@yandex.ru>
2023-11-26 14:42:34 +0300

    add login template

--- src/start.py
+++ src/start.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template
-import src.config as config
+from flask import Flask, render_template, request
+import config as config #src.config to config
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        # Здесь может быть проверка данных пользователя
+
+        return f'Добро пожаловать, {username}!'
+    
+    return render_template('login.html')
 
 if __name__ == '__main__':
     config.load_configurate('config.json')
