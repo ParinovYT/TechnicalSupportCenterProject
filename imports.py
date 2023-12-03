e2bd09fc143816ded7bae7c6d391225c717a1072
A0n1x <pakshin.2018@gmail.com>
2023-12-03 21:11:38 +0700

    fix

--- imports.py
+++ imports.py
@@ -2,6 +2,7 @@ from flask import Flask, Blueprint, redirect, render_template, request, session,
 from src.core.classes.user import User
 from src.core.classes.report import Report
 from src.core.classes.mysql import MySqlBase
+from src.core.classes.template_issue import TemplateIssue
 from src.tools import generate_password, getConfigurate, is_valid_password, is_valid_username
 from routes import routes
 from src.examples_ui import examples_ui
